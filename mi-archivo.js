24ab58bbbc7f36517c68a9520dae8d4c21a423e7
bryandavila <123339631+bryandavila@users.noreply.github.com>
2023-09-28 19:55:20 -0600

    Update mi-archivo.js
    Se agregaron comentarios para realizarlo de forma manual

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,3 +1,5 @@
 console.log('Hola mundo')
-//Este es un ejercicio de git y github
+//Aca se puede poner un mensaje x, ejemplo este es un ejercicio de git y github
+//Los comandos que se utilizan son: git commit -m "Mensaje sobre el cambio a realizar"
+//Luego $ git push
 console.log('Bryan Andres Davila Mendez')
